2f16eb1abdca11bd3341df6fdbbc2240d0286e38
Michael Mulholland <mulhollandm@sympatico.ca>
2018-11-16 14:49:19 -0500

    defined constants

--- src/micazuela/Constants.java
+++ src/micazuela/Constants.java
@@ -5,5 +5,8 @@ class Constants
 	/* Constants */
 	// Define constants as static
 	// Example: protected final static double realConstant = 8.0;
-
+	protected static final double COOK_SALARY = 100.0;
+	protected static final double WAITER_SALARY = 60.0;
+	protected static final double OVERHEAD_COST = 300.0;
+	
 }
