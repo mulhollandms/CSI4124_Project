3e5c8150b04b7db086123af537e7ad457480a315
Michael Mulholland <mulhollandm@sympatico.ca>
2018-11-23 20:15:46 -0500

    minor changes

--- src/Experiment.java
+++ src/Experiment.java
@@ -36,7 +36,7 @@ class Experiment
             balkCount[i]=simModel.output.countCustomerGroupBalking;
             waitTime[i]=simModel.output.avgTimeWaiting();
             timeSpent[i]=simModel.output.avgTimeSpent();
-            System.out.printf("*Terminated simulation run %d*",i+1);
+            System.out.printf("*Terminated simulation run %d*\n",i+1);
             // See examples for hints on collecting output
             // and developping code for analysis
        }
