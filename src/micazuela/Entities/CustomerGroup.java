a24c5a4ae558f09ee6262975a04306ee6066a12e
Michael Mulholland <mulhollandm@sympatico.ca>
2018-11-26 17:46:30 -0500

    corrected customergroup size to be type int

--- src/micazuela/Entities/CustomerGroup.java
+++ src/micazuela/Entities/CustomerGroup.java
@@ -1,6 +1,6 @@
 package micazuela.entities;
 
 public class CustomerGroup{
-    public double size; //size of the group of customers
+    public int size; //size of the group of customers
     public double arrivalTime;
 }
